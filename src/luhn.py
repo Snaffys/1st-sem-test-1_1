1c8be3491ffa7d0cf8533632c962bf77b21f4656
Dan <grezindanil@gmail.com>
2025-10-27 16:16:45 +0300

    Add input handling
    - Card number should only consist of digits
    - Length of card number must be >= 16

--- src/luhn.py
+++ src/luhn.py
@@ -1,6 +1,15 @@
 def luhn_check(card_number):
     card_number_str = str(card_number).replace(" ", "")
-    digits = [int(d) for d in card_number_str if d.isdigit()]
+    
+    if (not card_number_str.isdigit()):
+        print("Error: must contain only digits!")
+        return None
+
+    if(len(card_number_str) < 16):
+        print("Error: must contain at least 16 digits!")
+        return None
+
+    digits = [int(d) for d in card_number_str]
     control = digits.pop()
 
     parity = (len(digits) + 1) % 2
