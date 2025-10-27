8379cf47e6d16d66daf5f240a4a882a7c517ad6e
Dan <grezindanil@gmail.com>
2025-10-27 16:02:46 +0300

    Fix luhn algorithm
    Bugs:
    - Wrong parity calculation
    - Wrong doubled calculation
    - Wrong else case

--- src/luhn.py
+++ src/luhn.py
@@ -1,15 +1,17 @@
 def luhn_check(card_number):
     digits = [int(d) for d in str(card_number) if d.isdigit()]
     control = digits.pop()
-    parity = (len(digits)) % 2
+
+    parity = (len(digits) + 1) % 2
     total = 0
     for i in range(len(digits)):
+        doubled = digits[i]
+
         if i % 2 == parity:
-            doubled = digits[i] * 2
+            doubled = doubled * 2
             if doubled > 9:
-                doubled -= 9
-            total += doubled
-        else:
-            total += digits[i]
+                doubled = doubled - 9
+        total += doubled
+    
     return (total + control) % 10 == 0
 
