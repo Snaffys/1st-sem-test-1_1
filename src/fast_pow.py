5b8cb31f2589cd3ef6d798776f4c3f6f040bbfb2
Dan <grezindanil@gmail.com>
2025-10-27 16:20:59 +0300

    Format code

--- src/fast_pow.py
+++ src/fast_pow.py
@@ -1,27 +1,27 @@
 def fast_pow(number, power):
-    if (not isinstance(number, int)):
-        printf("Error: number is not an integer!")
+    if not isinstance(number, int):
+        print("Error: number is not an integer!")
         return None
-    if (not isinstance(power, int)):
-        printf("Error: power is not an integer!")
+    if not isinstance(power, int):
+        print("Error: power is not an integer!")
         return None
     if power < 0:
-        printf("Error: negative value!")
+        print("Error: negative value!")
         return None
 
     if power == 0:
         return 1
     if power == 1:
         return number
-    
+
     result = 1
     base = number
 
     while power > 0:
-        if (power % 2 == 1):
+        if power % 2 == 1:
             result *= base
         base *= base
         power //= 2
 
     return result
-    
+
--- src/luhn.py
+++ src/luhn.py
@@ -1,11 +1,11 @@
 def luhn_check(card_number):
     card_number_str = str(card_number).replace(" ", "")
-    
-    if (not card_number_str.isdigit()):
+
+    if not card_number_str.isdigit():
         print("Error: must contain only digits!")
         return None
 
-    if(len(card_number_str) < 16):
+    if len(card_number_str) < 16:
         print("Error: must contain at least 16 digits!")
         return None
 
@@ -22,6 +22,6 @@ def luhn_check(card_number):
             if doubled > 9:
                 doubled = doubled - 9
         total += doubled
-    
+
     return (total + control) % 10 == 0
 
