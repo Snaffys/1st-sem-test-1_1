142d714b1dc3676c5cc375acc824e6369edf5edd
Dan <grezindanil@gmail.com>
2025-10-27 15:52:29 +0300

    Format function and variables names

--- src/luhn.py
+++ src/luhn.py
@@ -1,7 +1,7 @@
-def luhnСheck(cardNumber):
-    digits = [int(d) for d in str(cardNumber) if d.isdigit()]
+def luhn_check(card_number):
+    digits = [int(d) for d in str(card_number) if d.isdigit()]
     control = digits.pop()
-    parity = (len(digits))%2
+    parity = (len(digits)) % 2
     total = 0
     for i in range(len(digits)):
         if i % 2 == parity:
@@ -12,3 +12,4 @@ def luhnСheck(cardNumber):
         else:
             total += digits[i]
     return (total + control) % 10 == 0
+
